6b7f9a516f801fe4c24f6c6716f1f7cd7eb5493f
lukacafuta <luka.cafuta@gmail.com>
2024-10-02 21:43:00 +0200

    created basic positioning for Home

--- Frontend/src/routes/Home.jsx
+++ Frontend/src/routes/Home.jsx
@@ -4,16 +4,17 @@ import HeroSection from "../components/sections/HeroSection.jsx";
 import ExcuseSection from "../components/sections/ExcuseSection.jsx";
 import EndpointSection from "../components/sections/EndpointSection.jsx";
 import AboutMeSection from "../components/sections/AboutMeSection.jsx";
+import {HomeStyled} from "../styles/globalStyles.js";
 
 export default function Home() {
     return (
-        <>
+        <HomeStyled>
             <Header/>
             <HeroSection/>
             <ExcuseSection/>
             <EndpointSection/>
             <AboutMeSection/>
             <Footer/>
-        </>
+        </HomeStyled>
     )
 }
