623fe31e0c2779dcfa4c05ab8ea2c0fcc09d5a58
lukacafuta <luka.cafuta@gmail.com>
2024-10-09 14:02:01 +0200

    hero section setup

--- Frontend/src/components/sections/HeroSection.jsx
+++ Frontend/src/components/sections/HeroSection.jsx
@@ -1,7 +1,13 @@
+import {HeroSectionStyled} from "../../styles/globalStyles.js";
+
 export default function HeroSection() {
     return (
-        <div>
-            Hero Section
-        </div>
+        <HeroSectionStyled>
+            <div className="hero-content">
+                <h1>Lets get Eaasy!</h1>
+                <h2>Excuse as a Service</h2>
+                <button>TRY IT</button>
+            </div>
+        </HeroSectionStyled>
     )
 }
