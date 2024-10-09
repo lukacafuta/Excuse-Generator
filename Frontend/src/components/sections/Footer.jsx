c62982df105279083029050cd99e4526c65a1f65
lukacafuta <luka.cafuta@gmail.com>
2024-10-09 13:34:38 +0200

    imported StackRectangleStyled wrapped it around logos

--- Frontend/src/components/sections/Footer.jsx
+++ Frontend/src/components/sections/Footer.jsx
@@ -1,11 +1,13 @@
-import {FooterStyled} from "../../styles/globalStyles.js";
+import {FooterStyled, StackRectangleStyled} from "../../styles/globalStyles.js";
 
 export default function Footer() {
     return (
         <FooterStyled>
-            <img src="/react-logo.svg" alt="React Logo"/>
-            <img src="/django-logo.svg" alt="Django Logo"/>
-            <img src="/sqlite-logo.svg" alt="SQLite Logo"/>
+            <StackRectangleStyled>
+                <img src="/react-logo.svg" alt="React Logo"/>
+                <img src="/django-logo.svg" alt="Django Logo"/>
+                <img src="/sqlite-logo.svg" alt="SQLite Logo"/>
+            </StackRectangleStyled>
         </FooterStyled>
     )
 }
