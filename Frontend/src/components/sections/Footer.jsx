77300bd4a95552bf3e0852245e8eb158ea2d341e
lukacafuta <luka.cafuta@gmail.com>
2024-10-09 13:09:58 +0200

    added sqlite logo

--- Frontend/src/components/sections/Footer.jsx
+++ Frontend/src/components/sections/Footer.jsx
@@ -3,6 +3,7 @@ export default function Footer() {
         <div>
             <img src="/react-logo.svg" alt="React Logo"/>
             <img src="/django-logo.svg" alt="Django Logo"/>
+            <img src="/sqlite-logo.svg" alt="SQLite Logo"/>
         </div>
     )
 }
