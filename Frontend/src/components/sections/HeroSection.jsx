79eab478ac6001cf205505892109ea46475447bf
lukacafuta <luka.cafuta@gmail.com>
2024-10-10 20:19:09 +0200

    added comments

--- Frontend/src/components/sections/HeroSection.jsx
+++ Frontend/src/components/sections/HeroSection.jsx
@@ -4,8 +4,8 @@ export default function HeroSection() {
     return (
         <HeroSectionStyled>
             <div className="hero-content">
-                <h1>Lets get Eaasy!</h1>
-                <h2>Excuse as a Service</h2>
+                <h1>Lets get Eaasy!</h1>   {/*in index css*/}
+                <h2>Excuse as a Service</h2>    {/*in index css*/}
                 <button>TRY IT</button>
             </div>
         </HeroSectionStyled>
