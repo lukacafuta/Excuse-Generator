698acfebcde7c23bc7b50ab0559c529ca37e7731
lukacafuta <luka.cafuta@gmail.com>
2024-10-11 23:14:03 +0200

    about me setup, rough content

--- Frontend/src/components/sections/AboutMeSection.jsx
+++ Frontend/src/components/sections/AboutMeSection.jsx
@@ -3,7 +3,18 @@ import {AboutMeSectionStyled} from "../../styles/globalStyles.js";
 export default function AboutMeSection() {
     return (
         <AboutMeSectionStyled>
-            About me section
+            <h2>About me</h2>
+            <h3>Hi, I am Luka - a Full Stack Dev</h3>
+            <div>
+                <img src="/luka-profile-photo.png" alt="profile photo"/>
+                <p>
+                    Lorem ipsum dolor sit amet consectetur adipiscing elit venenatis hac dictum, vitae augue arcu
+                    penatibus pharetra aenean vel tempus suspendisse porta, ridiculus ullamcorper semper lobortis
+                    bibendum donec ornare ut mattis. Condimentum viverra fames pulvinar molestie quis maecenas
+                    scelerisque congue mollis a ante urna ac.
+                </p>
+            </div>
+            <img src="/linkedin-logo.svg" alt="linkedin logo"/>
         </AboutMeSectionStyled>
     )
 }
