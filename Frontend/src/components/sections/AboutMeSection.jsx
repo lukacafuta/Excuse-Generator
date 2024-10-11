7703040c28c97524f63a1d7236125a079eedfdea
lukacafuta <luka.cafuta@gmail.com>
2024-10-11 23:29:50 +0200

    moved <p> and img into separat div

--- Frontend/src/components/sections/AboutMeSection.jsx
+++ Frontend/src/components/sections/AboutMeSection.jsx
@@ -5,16 +5,18 @@ export default function AboutMeSection() {
         <AboutMeSectionStyled>
             <h2>About me</h2>
             <h3>Hi, I am Luka - a Full Stack Dev</h3>
-            <div>
+            <div className="about-me-content">
                 <img src="/luka-profile-photo.png" alt="profile photo"/>
-                <p>
-                    Lorem ipsum dolor sit amet consectetur adipiscing elit venenatis hac dictum, vitae augue arcu
-                    penatibus pharetra aenean vel tempus suspendisse porta, ridiculus ullamcorper semper lobortis
-                    bibendum donec ornare ut mattis. Condimentum viverra fames pulvinar molestie quis maecenas
-                    scelerisque congue mollis a ante urna ac.
-                </p>
+                <div className="about-me-text-linkedin">
+                    <p>
+                        Lorem ipsum dolor sit amet consectetur adipiscing elit venenatis hac dictum, vitae augue arcu
+                        penatibus pharetra aenean vel tempus suspendisse porta, ridiculus ullamcorper semper lobortis
+                        bibendum donec ornare ut mattis. Condimentum viverra fames pulvinar molestie quis maecenas
+                        scelerisque congue mollis a ante urna ac.
+                    </p>
+                    <img src="/linkedin-logo.svg" alt="linkedin logo"/>
+                </div>
             </div>
-            <img src="/linkedin-logo.svg" alt="linkedin logo"/>
         </AboutMeSectionStyled>
     )
 }
