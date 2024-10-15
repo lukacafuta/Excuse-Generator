fb2048f1bb2416bc25f7000871be422ce6271c72
lukacafuta <luka.cafuta@gmail.com>
2024-10-15 23:08:02 +0200

    divided section into header and tab part

--- Frontend/src/components/sections/EndpointSection.jsx
+++ Frontend/src/components/sections/EndpointSection.jsx
@@ -3,11 +3,11 @@ import {EndpointSectionStyled} from "../../styles/globalStyles.js";
 export default function EndpointSection() {
     return (
         <EndpointSectionStyled>
-            <h2>Excuse Documentation</h2>
-            <h3>Crud your way through some excuses</h3>
-            <div className="endpoints">
-
+            <div className="endpoints-header">
+                <h2>Excuse Documentation</h2>
+                <h3>Crud your way through some excuses</h3>
             </div>
+            {/*<TabContainer/>*/}
 
         </EndpointSectionStyled>
     )
