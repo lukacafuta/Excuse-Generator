e58ed87309aed58e36826834bef3f69ee3518cdf
lukacafuta <luka.cafuta@gmail.com>
2024-10-16 09:16:10 +0200

    divided content tab into description, endpoint and output part

--- Frontend/src/components/tabs/TabContent.jsx
+++ Frontend/src/components/tabs/TabContent.jsx
@@ -1,11 +1,16 @@
 import {TabContentStyled} from "../../styles/globalStyles.js";
+import TabDatabaseOutput from "./TabDatabaseOutput.jsx";
 
 export default function TabContent() {
     return (
         <TabContentStyled>
-            Description
-            Endpoint
-            TabExcuseApi
+            <div className="description">
+                Gotta get all of them excuses
+            </div>
+            <div className="endpoint">
+                https://excuse-generator-api.herokuapp.com/api/excuse/
+            </div>
+            <TabDatabaseOutput/>
         </TabContentStyled>
     )
 }
