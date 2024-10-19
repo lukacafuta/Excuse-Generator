d4639a04d46180b646fe79d96768457893dd4ad7
lukacafuta <luka.cafuta@gmail.com>
2024-10-19 23:47:37 +0200

    font styling

--- Frontend/src/components/sections/AboutMeSection.jsx
+++ Frontend/src/components/sections/AboutMeSection.jsx
@@ -4,7 +4,7 @@ export default function AboutMeSection() {
     return (
         <AboutMeSectionStyled>
             <h2>About me</h2>
-            <h3>Hi, I am Luka - a Full Stack Dev</h3>
+            <div className="about-me-subtitle">Hi, I am Luka - a Full Stack Dev</div>
             <div className="about-me-content">
                 <div className="profile-photo-wrapper">
                     <img className="profile-photo" src="/luka-profile-photo.png" alt="profile photo"/>
@@ -13,8 +13,7 @@ export default function AboutMeSection() {
                     <div className="text">
                         Experienced in the healthcare sector, administrative skills, and a strong customer focus.<br/>
                         In addition to solid computer science and programming knowledge (JavaScript, Python, SQL), I
-                        have extensive experience in customer service and organization.<br/>
-                        I speak fluent German and English.
+                        have extensive experience in customer service and organization.
                     </div>
                     <img src="/linkedin-logo.svg" alt="linkedin logo"/>
                 </div>
