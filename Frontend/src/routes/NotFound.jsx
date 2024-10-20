4f1942f91ff2d4bea7455b9a89cf69b8ee24072e
lukacafuta <luka.cafuta@gmail.com>
2024-10-20 18:22:10 +0200

    added NotFound, but it's not working properly

---
+++ Frontend/src/routes/NotFound.jsx
@@ -0,0 +1,8 @@
+export default function NotFound() {
+    return (
+        <>
+            <h1>Page not found</h1>
+            <p>Don't worry, you'll nail it next time!</p>
+        </>
+    )
+}
--- Frontend/src/routes/index.jsx
+++ Frontend/src/routes/index.jsx
@@ -1,6 +1,7 @@
 import {BrowserRouter, Route, Routes} from "react-router-dom";
 import Home from "./Home.jsx";
 import TabContent from "../components/tabs/TabContent.jsx";
+import NotFound from "./NotFound.jsx";
 
 
 export default function Router() {
@@ -10,6 +11,7 @@ export default function Router() {
                 <Route path={"/"} element={<Home/>}>
                     <Route path={":id"} element={<TabContent/>}/>
                 </Route>
+                <Route path={"*"} element={<NotFound/>}/>
             </Routes>
         </BrowserRouter>
     )
