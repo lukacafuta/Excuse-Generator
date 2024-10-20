792bd5f465ab37ddd13412cf8b7ab0cfd772789d
lukacafuta <luka.cafuta@gmail.com>
2024-10-20 14:31:28 +0200

    cleaned dummy endpoints

--- Frontend/src/common/dummyEndpoints.js
+++ Frontend/src/common/dummyEndpoints.js
@@ -4,18 +4,20 @@ const dummyEndpoints = [
         title: "Get all Excuses",
         description: "Get all excuses in the database",
         endpoint: "/excuse/",
-        content:
-            `
-            [{<br>
-            &emsp;&emsp;"id": 1,<br>
-            &emsp;&emsp;"category": "School",<br>
-            &emsp;&emsp;"excuse": "My homework turned into origami after I accidentally spilled water on it."<br>
-            &emsp;}, {<br>
-            &emsp;&emsp;"id": 2,<br>
-            &emsp;&emsp;"category": "School",<br>
-            &emsp;&emsp;"excuse": "I thought today was Saturday, so I didn't show up."<br>
-            }]
-            `
+        content: `
+        [
+            {
+                "id": 1,
+                "category": "School",
+                "excuse": "My homework turned into origami after I accidentally spilled water on it."
+            },
+            {
+                "id": 2,
+                "category": "School",
+                "excuse": "I thought today was Saturday, so I didn't show up."
+            }
+        ]
+        `
     },
     {
         id: "get-specific-excuse",
@@ -23,51 +25,55 @@ const dummyEndpoints = [
         description: "Get a specific excuse by ID",
         endpoint: "/excuse/{ID}",
         example: "/excuse/1",
-        content:
-            `
-            [{<br>
-            &emsp;&emsp;"id": 1,<br>
-            &emsp;&emsp;"category": "School",<br>
-            &emsp;&emsp;"excuse": "My homework turned into origami after I accidentally spilled water on it."<br>
-            }]
-            `
+        content: `
+        [
+            {
+                "id": 1,
+                "category": "School",
+                "excuse": "My homework turned into origami after I accidentally spilled water on it."
+            }
+        ]
+        `
     },
     {
         id: "post-an-excuse",
         title: "Post an Excuse",
         description: "Add an Excuse to the database",
         endpoint: "/excuse/",
-        content:
-            `
-            [{<br>
-            &emsp;&emsp;"id": 1,<br>
-            &emsp;&emsp;"category": "School",<br>
-            &emsp;&emsp;"excuse": "My homework turned into origami after I accidentally spilled water on it."<br>
-            }]
-            `
-    }, {
+        content: `
+        [
+            {
+                "id": 1,
+                "category": "School",
+                "excuse": "My homework turned into origami after I accidentally spilled water on it."
+            }
+        ]
+        `
+    },
+    {
         id: "patch-an-excuse",
         title: "Patch an Excuse",
         description: "Partial update of an Excuse in the database",
         endpoint: "/excuse/",
-        content:
-            `
-            [{<br>
-            &emsp;&emsp;"excuse": "My homework turned into origami after I accidentally spilled COFFEE on it.",<br>
-            &emsp;}]
-            `
-    }, {
+        content: `
+        [
+            {
+                "excuse": "My homework turned into origami after I accidentally spilled COFFEE on it."
+            }
+        ]
+        `
+    },
+    {
         id: "delete-an-excuse",
         title: "Delete an Excuse",
         description: "Delete a specific Excuse by ID",
         endpoint: "/excuse/{ID}",
         example: "/excuse/1",
-        content:
-            `
-            [{<br>
-            {"message": "Excuse deleted successfully."}<br>
-            }]
-            `
+        content: `
+        {
+            "message": "Excuse deleted successfully."
+        }
+        `
     },
     {
         id: "search-for-excuses",
@@ -75,15 +81,16 @@ const dummyEndpoints = [
         description: "Get all excuses from the database, containing a string in category or excuse",
         endpoint: "/excuse/search/{search_term}",
         example: "/excuse/search/homework/",
-        content:
-            `
-            [{<br>
-            &emsp;&emsp;"id": 1,<br>
-            &emsp;&emsp;"category": "School",<br>
-            &emsp;&emsp;"excuse": "My homework turned into origami after I accidentally spilled water on it."<br>
-            }]
-            `
+        content: `
+        [
+            {
+                "id": 1,
+                "category": "School",
+                "excuse": "My homework turned into origami after I accidentally spilled water on it."
+            }
+        ]
+        `
     }
-]
+];
 
 export default dummyEndpoints;
