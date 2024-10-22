07a0f58998b98eb7a70b5fc0e79cc3481d4101f9
lukacafuta <luka.cafuta@gmail.com>
2024-10-22 23:20:41 +0200

    formatting the JSON data for better readability

--- Frontend/src/components/tabs/TabDatabaseOutput.jsx
+++ Frontend/src/components/tabs/TabDatabaseOutput.jsx
@@ -1,10 +1,20 @@
 import {TabDatabaseOutputStyled} from "../../styles/globalStyles.js";
 
 export default function TabDatabaseOutput({tab}) {
+    let formattedContent;
+
+    try {
+        // parsing and formatting the content as JSON
+        const parsedContent = JSON.parse(tab.content.trim());
+        formattedContent = JSON.stringify(parsedContent, null, 2); // pretty print JSON with 2 spaces of indentation
+    } catch (error) {
+        // if content is not JSON, just display it as it is
+        formattedContent = tab.content;
+    }
 
     return (
         <TabDatabaseOutputStyled>
-            {tab.content}
+            <pre>{formattedContent}</pre>
         </TabDatabaseOutputStyled>
     )
 }
