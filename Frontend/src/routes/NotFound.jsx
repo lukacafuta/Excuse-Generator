de13bf31855c57c0140c7602bbae025afef5477b
lukacafuta <luka.cafuta@gmail.com>
2024-10-24 20:55:31 +0200

    updated styling

--- Frontend/src/routes/NotFound.jsx
+++ Frontend/src/routes/NotFound.jsx
@@ -2,7 +2,7 @@ export default function NotFound() {
     return (
         <>
             <h1>Page not found</h1>
-            <p>Don't worry, you'll nail it next time!</p>
+            <p>Do not worry, you will nail it next time!</p>
         </>
     )
 }
--- Frontend/src/routes/index.jsx
+++ Frontend/src/routes/index.jsx
@@ -3,15 +3,14 @@ import Home from "./Home.jsx";
 import TabContent from "../components/tabs/TabContent.jsx";
 import NotFound from "./NotFound.jsx";
 
-
 export default function Router() {
     return (
         <BrowserRouter>
             <Routes>
-                <Route path={"/"} element={<Home/>}>
-                    <Route path={":id"} element={<TabContent/>}/>
+                <Route path="/" element={<Home/>}>
+                    <Route path=":id" element={<TabContent/>}/>
                 </Route>
-                <Route path={"*"} element={<NotFound/>}/>
+                <Route path="*" element={<NotFound/>}/>
             </Routes>
         </BrowserRouter>
     )
