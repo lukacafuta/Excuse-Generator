24c8159183190bac67b2f9671df8f7558db07b25
lukacafuta <luka.cafuta@gmail.com>
2024-11-14 01:12:20 +0100

    added serializer, urls, views in category

--- Backend/excuse/serializers.py
+++ Backend/excuse/serializers.py
@@ -4,6 +4,7 @@ from excuse.models import Excuse
 
 
 class ExcuseSerializer(serializers.ModelSerializer):
+
     class Meta:
         model = Excuse      # this is the model serializer looks at
-        fields = '__all__'  # these are the fields that serializer will return
+        fields = ['id', 'excuse_text', 'author', 'likes']  # these are the fields that serializer will return
--- Backend/excuse/urls.py
+++ Backend/excuse/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
 
-from excuse.views import ListCreateExcusesView
+from excuse.views import ListCreateExcusesView, RetrieveUpdateDeleteExcusesView
 
 urlpatterns = [
     path('', ListCreateExcusesView.as_view()),
+    path('<int:pk>/', RetrieveUpdateDeleteExcusesView.as_view())
 ]
