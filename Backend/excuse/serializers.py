7042ce991b66c4f7f47eecf0be09fe71c7641665
lukacafuta <luka.cafuta@gmail.com>
2024-11-17 14:15:09 +0100

    updated the model, serializer, used concrete views, added random excuse

--- Backend/excuse/serializers.py
+++ Backend/excuse/serializers.py
@@ -7,4 +7,10 @@ class ExcuseSerializer(serializers.ModelSerializer):
 
     class Meta:
         model = Excuse      # this is the model serializer looks at
-        fields = ['id', 'excuse_text', 'author', 'likes']  # these are the fields that serializer will return
+        fields = ['id', 'excuse_text', 'nickname', 'category', 'likes', 'created']  # these are the fields that serializer will return
+
+    def validate_category(self, value):
+        valid_categories = [choice[0] for choice in Excuse.CATEGORY_CHOICES]
+        if value not in valid_categories:
+            raise serializers.ValidationError(f"'{value}' is not a valid category. Choose from {valid_categories}.")
+        return value
--- Backend/excuse/urls.py
+++ Backend/excuse/urls.py
@@ -1,8 +1,8 @@
 from django.urls import path
-
-from excuse.views import ListCreateExcusesView, RetrieveUpdateDeleteExcusesView
+from excuse.views import ListCreateExcusesView, RetrieveUpdateDeleteExcusesView, RandomExcuseView
 
 urlpatterns = [
-    path('', ListCreateExcusesView.as_view()),
-    path('<int:pk>/', RetrieveUpdateDeleteExcusesView.as_view())
+    path('', ListCreateExcusesView.as_view(), name='list_create_excuses'),
+    path('<int:pk>/', RetrieveUpdateDeleteExcusesView.as_view(), name='retrieve_update_delete_excuse'),
+    path('random/<str:category>/', RandomExcuseView.as_view(), name='random_excuse'),
 ]
